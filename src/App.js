ebfa1022beec18aa0be3f4da218a9f88df5c7d29
KenTandrian <kenricktan11@gmail.com>
2022-01-02 16:25:20 +0700

    Add AI Detector Feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,164 @@
 import React from 'react';
+import Particles from 'react-tsparticles';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
+console.log('Clarifai', Clarifai);
+
+const app = new Clarifai.App({
+  apiKey: '66feb1d09b85417cbd5ddaca5d558e47'
+});
+
+const particlesOptions = {
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        area: 1000,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.2,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+}
+
+const particlesInit = (main) => {
+  //console.log(main);
+};
+
+const particlesLoaded = (container) => {
+  //console.log(container);
+};
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+    //console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    // The parameters are Model ID and a sample photo
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Particles
+          id="tsparticles"
+          className='particles'
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={particlesOptions}
+        />
+        <div className='flex justify-between mv4 mr3'>
+          <Logo />
+          <Navigation />
+        </div> 
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-            
+            <Tilt 
+                className="Tilts br2 shadow-2 w-150" 
+                style={{height: '150px', width: '150px'}}
+                tiltMaxAngleX={35}
+                tiltMaxAngleY={35}
+            >
+                <div className="pa3">
+                    <img style={{paddingTop: '3px'}} src={brain} alt="Logo" />
+                </div>
+            </Tilt>
         </div>
     )
 }
